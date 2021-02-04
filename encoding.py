b483fd6aa91da86ba4d94a47fc8108112e279417
dan480 <sak_2003@mail.ru>
2021-02-04 21:07:06 +0300

    Update encoding.py

--- encoding.py
+++ encoding.py
@@ -1,14 +1,17 @@
-key = -2
-
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
 
 def encoding_func(string, alphabet, key):
-    al = alphabet
-    al_encod = al[key:] + al[:key]
+    alphabet_encod = alphabet[key:] + alphabet[:key]
     str_out = ''
     for i in string:
-        if i not in al:
+        if i.lower() not in alphabet:
             str_out += i
         else:
-            ch = al.index(i.lower())
-            str_out += str(al_encod[ch])
+            if i.isupper():
+                ch = alphabet.index(i.lower())
+                str_out += str(alphabet_encod[ch]).upper()
+            else:
+                ch = alphabet.index(i.lower())
+                str_out += str(alphabet_encod[ch])
     print(str_out)
