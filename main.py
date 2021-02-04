8104243825db4ee1e3eda7f48295a939d7135d29
dan480 <sak_2003@mail.ru>
2021-02-04 21:11:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,33 +8,33 @@ import sys
 from encoding import encoding_func, key
 from decoding import decoding_func, key
 
+
+key = -2
 alphabet = []
 
-for root, dirs, files in os.walk("./data"):  
-    for filename in files:
-        print(filename)
+#for root, dirs, files in os.walk("./data"):  
+#    for filename in files:
+#        print(filename)
 
-with open('data/russian.csv', newline='') as f:
+with open('data/english.csv', newline='') as f:
     reader = csv.reader(f)
     for row in reader:
         alphabet.extend(row)
 
 
-if __name__ == "__main__":
-    if len (sys.argv) <= 2:
-        print ("Missing parameters!")
+if len (sys.argv) <= 2:
+    print ("Missing parameters!")
+    sys.exit(1)
+else:
+    if len (sys.argv) > 3:
+        print ("Error. Expected 1 parameter.")
         sys.exit(1)
-    else:
-        if len (sys.argv) > 3:
-            print ("Error. Expected 1 parameter.")
-            sys.exit(1)
-            
-        if len (sys.argv) == 3:
+    elif len (sys.argv) == 3:
             param_name = sys.argv[1]
             param_string = sys.argv[2]         
-            if (param_name == "encoding"):
+            if param_name  in ["encoding", "-e"]:
                 encoding_func(param_string, alphabet, key)
-            elif (param_name == "decoding"):
+            elif param_name in ["decoding", "-d"]:
                 decoding_func(param_string, alphabet, key)
             else:
                 print ("Error. Unknown parameter'{}'".format(param_name))
