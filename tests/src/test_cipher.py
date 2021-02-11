62257734242ba7a3cbfb625c2bdd127cb56bfa87
dan480 <sak_2003@mail.ru>
2021-02-11 17:09:53 +0300

    Update file

---
+++ tests/src/test_cipher.py
@@ -0,0 +1,2 @@
+import pytest
+from src.encoding import encoding_func
--- tests/test_main.py
+++ tests/test_main.py
@@ -3,6 +3,5 @@ from src.encoding import encoding_func
 
 
 def test_encoding_func():
-    result = encoding_func('', 'alphabet', 1)
-
-    assert result == ''
+    result = encoding_func("", "alphabet", 2)
+    assert result == ""
