46be399b36e5d66df04c8c3eebed4ca24a1f8a7d
dan480 <sak_2003@mail.ru>
2021-02-28 13:21:12 +0300

    Add docstring and check the validity of the data

--- src/encoding.py
+++ src/encoding.py
@@ -8,10 +8,14 @@ Encodes text by shifting the list with the original alphabet by 2 steps to the l
 
 
 def encoding_func(string, alphabet, key):
-    if string[0].lower() not in alphabet:
-        raise ValueError("The argument has an inappropriate value")
+    # Check the string for a valid value.
+    for char in string.lower():
+        if char.isalpha() and char not in alphabet:
+            raise ValueError("The argument has an inappropriate value")
+    # We get a new dictionary by shifting by a given step.
     alphabet_encode = alphabet[key:] + alphabet[:key]
     str_out = ""
+    # We translate each letter of the string in accordance with the new encoding.
     for letter in string:
         if letter.lower() not in alphabet:
             str_out += letter
