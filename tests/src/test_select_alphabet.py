b7cd1f38eba9336b14e1b64c5baeeb7610ef5c84
dan480 <sak_2003@mail.ru>
2021-02-28 16:36:41 +0300

    Update test

--- tests/src/test_select_alphabet.py
+++ tests/src/test_select_alphabet.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
-import argparse
 import unittest
-from src.select_alphabet import get_input, select_alphabet
+from src.select_alphabet import select_alphabet
 
 
-# Test cases to test 'encoding' methods
+# Test cases to test 'select_alphabet' methods
 class TestCipher(unittest.TestCase):
-    def test_get_input(self):
-        pass
-
     def test_select_alphabet(self):
         self.assertRaises(TypeError, select_alphabet, 1)
 
