4a35fbf851f30bdd0ec4b8a54a203d2d1a6bc3e7
dan480 <sak_2003@mail.ru>
2021-02-28 17:02:22 +0300

    Add docstring

--- tests/src/test_decoding.py
+++ tests/src/test_decoding.py
@@ -3,22 +3,25 @@
 
 import unittest
 from src.decoding import decoding_func
-from src.cipher import select_alphabet
+from src.select_alphabet import select_alphabet
 
 alphabet = select_alphabet("w")
 key = 2
 
 
-# Test cases to test 'encoding' methods
+# Test cases to test 'decoding' methods
 class TestEncode(unittest.TestCase):
+    # Expected Result Test
     def test_decoding(self):
         self.assertEqual(decoding_func("y", alphabet, key), "w")
         self.assertEqual(decoding_func("c", alphabet, key), "a")
 
+    # Checking for an invalid value
     def test_value(self):
         self.assertRaises(ValueError, decoding_func, "й", alphabet, key)
         self.assertRaises(ValueError, decoding_func, "я", alphabet, key)
 
+    # Checking for the wrong input data type
     def test_type(self):
         self.assertRaises(TypeError, decoding_func, 1, alphabet, key)
 
--- tests/src/test_encoding.py
+++ tests/src/test_encoding.py
@@ -3,7 +3,7 @@
 
 import unittest
 from src.encoding import encoding_func
-from src.cipher import select_alphabet
+from src.select_alphabet import select_alphabet
 
 alphabet = select_alphabet("w")
 key = 2
@@ -11,14 +11,17 @@ key = 2
 
 # Test cases to test 'encoding' methods
 class TestEncode(unittest.TestCase):
+    # Expected Result Test
     def test_decoding(self):
         self.assertEqual(encoding_func("w", alphabet, key), "y")
         self.assertEqual(encoding_func("a", alphabet, key), "c")
 
+    # Checking for an invalid value
     def test_value(self):
         self.assertRaises(ValueError, encoding_func, "й", alphabet, key)
         self.assertRaises(ValueError, encoding_func, "я", alphabet, key)
 
+    # Checking for the wrong input data type
     def test_type(self):
         self.assertRaises(TypeError, encoding_func, 1, alphabet, key)
 
